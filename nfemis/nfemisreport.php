3510099a95316e7194032a13343dd90255575905
sambedan koirala <sambedan@hotmail.com>
2015-03-01 15:51:30 +0545

    changes made in nfe report

--- nfemis/nfemisreport.php
+++ nfemis/nfemisreport.php
@@ -44,7 +44,9 @@ $result = mysql_query($query);
                             <td></td>
                             <th colspan="6">Agency Type</th>
                             <th colspan="4">Misc.</th>
-                            <th colspan="2">Facilitators</th>
+                            <th colspan="5">Facilitators</th>
+							<th colspan="12">Participants</th>
+														
                             
                         </tr>
                         <tr>
@@ -61,6 +63,23 @@ $result = mysql_query($query);
                             <th>SMC</th>
                             <th>Male</th>
                             <th>Female</th>
+							<th>< SLC</th>
+							<th>SLC</th>
+							<th>> SLC</th>
+							<th>Male</th>
+                            <th>Female</th>
+							<th>Dalit</th>
+							<th>Janjati</th>
+							<th>B/C</th>
+							<th>Others</th>
+							<th>Disabled</th>
+							<th>< 15</th>
+							<th>15-30</th>
+							<th>30-45</th>
+							<th>45-60</th>
+							<th>> 60</th>
+							
+							
                         </tr>
                         
                         <?php while ($row = mysql_fetch_assoc($result)){ ?>
@@ -79,14 +98,36 @@ $result = mysql_query($query);
                                 
                                 <?php 
                                     //now get the facilitator count
-                                    $result1=mysql_query("SELECT IFNULL(SUM(IF(sex=1,1,0)),0) AS 'male',IFNULL(SUM(IF(sex=2,1,0)),0) AS 'female' 
+                                    $result1=mysql_query("SELECT IFNULL(SUM(IF(sex=1,1,0)),0) AS 'male',IFNULL(SUM(IF(sex=2,1,0)),0) AS 'female',IFNULL(SUM(IF(education<=4,1,0)),0) AS '<slc',IFNULL(SUM(IF(education=5,1,0)),0) AS 'slc',IFNULL(SUM(IF(education>5,1,0)),0) AS '>slc'
                                                         from `nfe_facilitators` WHERE `center_id`=".$row['center_id']." AND `year`=".$row['year']."
                                                         AND `agency_type`=".$row['agency_type']);
                                     $facilitators=mysql_fetch_assoc($result1);
                                     echo "<td>".$facilitators['male']."</td>";
                                     echo "<td>".$facilitators['female']."</td>";
+									echo "<td>".$facilitators['<slc']."</td>";
+                                    echo "<td>".$facilitators['slc']."</td>";
+									echo "<td>".$facilitators['>slc']."</td>";
                                 ?>
-                                
+                                <?php 
+                                    //now get the participants count
+                                    $result1=mysql_query("SELECT IFNULL(SUM(IF(sex=1,1,0)),0) AS 'male',IFNULL(SUM(IF(sex=2,1,0)),0) AS 'female',IFNULL(SUM(IF(caste=1,1,0)),0) AS 'dalit',IFNULL(SUM(IF(caste=2,1,0)),0) AS 'janjati',IFNULL(SUM(IF(caste=3,1,0)),0) AS 'b/c',IFNULL(SUM(IF(caste=4,1,0)),0) AS 'others',IFNULL(SUM(IF(disability>=1,1,0)),0) AS 'disabled',IFNULL(sum(IF(age between 1 and 15,1,0)),0) AS 'below 15',IFNULL(sum(IF(age between 16 and 30,1,0)),0) AS '16-30',IFNULL(sum(IF(age between 31 and 45,1,0)),0) AS '30-45',IFNULL(sum(IF(age between 46 and 60,1,0)),0) AS '46-60',IFNULL(sum(IF(age>60,1,0)),0) AS 'greater than 60'
+                                                        from `nfe_participants` WHERE `center_id`=".$row['center_id']." AND `year`=".$row['year']."
+                                                        AND `agency_type`=".$row['agency_type']);
+                                    $participants=mysql_fetch_assoc($result1);
+                                    echo "<td>".$participants['male']."</td>";
+                                    echo "<td>".$participants['female']."</td>";
+									echo "<td>".$participants['dalit']."</td>";
+									echo "<td>".$participants['janjati']."</td>";
+									echo "<td>".$participants['b/c']."</td>";
+									echo "<td>".$participants['others']."</td>";
+									echo "<td>".$participants['disabled']."</td>";
+									echo "<td>".$participants['below 15']."</td>";
+									echo "<td>".$participants['16-30']."</td>";
+									echo "<td>".$participants['30-45']."</td>";
+									echo "<td>".$participants['46-60']."</td>";
+									echo "<td>".$participants['greater than 60']."</td>";
+									
+                                ?>
                             </tr>
                         <?php } ?>
                         
